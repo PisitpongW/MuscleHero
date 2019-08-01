5af700e5f9eac81a1bbeabe0b14ee8c4f3406c7e
Unknown <pisitpong.won@gmail.com>
2019-08-02 06:50:14 +0700

    Decorate all Lava Environment

--- MuscleHero/Assets/NoteController.cs
+++ MuscleHero/Assets/NoteController.cs
@@ -4,16 +4,7 @@ using UnityEngine;
 public class NoteController : MonoBehaviour 
 {
     public GameObject noteColor;
-	public GameObject noteBlack;
-	public GameObject noteBlue;
-	public GameObject noteCyan;
-	public GameObject noteGreen;
-	public GameObject noteGrey;
-	public GameObject noteMagenta;
-	public GameObject noteOrange;
-	public GameObject noteRed;
-	public GameObject noteWhite;
-	public GameObject noteYellow;
+	public GameObject noteLeft, noteMiddle, noteRight;
 	public Vector3 spawnValue;
 	public float spawnWait;
 	public float waitBefore;
@@ -33,9 +24,9 @@ public class NoteController : MonoBehaviour
 		while(true)
 		{
 			index = Random.Range(1,4);
-			if(index == 1){spawnValue.x = -3.71f; noteColor = noteCyan;}
-			else if(index == 2) {spawnValue.x = -0.275f; noteColor = noteOrange;}
-			else if(index == 3) {spawnValue.x = 3.16f; noteColor = noteRed;}
+			if(index == 1){spawnValue.x = -3.71f; noteColor = noteLeft;}
+			else if(index == 2) {spawnValue.x = -0.275f; noteColor = noteMiddle;}
+			else if(index == 3) {spawnValue.x = 3.16f; noteColor = noteRight;}
 			Vector3 spawnPosition = spawnValue;
 			Quaternion spawnRotation = Quaternion.identity;
 			Instantiate(noteColor, spawnPosition, spawnRotation);
